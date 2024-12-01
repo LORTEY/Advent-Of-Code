580d23cd6dadcc1b0212465cfe0e8b6dda289ac6
LORTEY <wnozka19@gmail.com>
2024-12-01 22:58:11 +0100

    first day first task

---
+++ 1.1/1.py
@@ -0,0 +1,24 @@
+import os
+
+path = os.path.join(os.path.dirname(__file__), "input.txt")
+
+f = open(path, "r")
+inp = f.read()
+
+inp = inp.replace("   ", "\n").split("\n")[0:-2]
+lista = []
+listb = []
+
+for i in range(0, len(inp)):
+	if i%2 == 0:
+		lista.append(inp[i])
+	else:
+		listb.append(inp[i])
+lista = sorted(lista)
+listb = sorted(listb)
+
+solution = 0
+for t in range(0, len(lista)):
+    solution += abs(int(lista[t]) - int(listb[t]))
+
+print(solution)
